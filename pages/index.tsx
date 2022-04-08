c10d21c5cc800879984a26bfb2f93df3138e4a91
shedrachIkenna <shedrach686@gmail.com>
2022-04-08 20:17:30 +0100

    Finished header component UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +9,8 @@ const Home: NextPage = () => {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <h1>This is Medium App</h1>
-    </div>
+      <Header />
+    </div> 
   )
 }
 
